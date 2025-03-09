073ef960e9a5af8f4adc4e9ed31429d2e1c2fef2
execriss <egc.criss@gmail.com>
2025-03-09 17:40:04 -0300

    instalacion de tailwind

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -10,7 +10,7 @@ const Balance = () => {
     
     return (
         <div>
-            <h3>Tu Balance</h3>
+            <h3 className='text-3xl font-bold underline text-center text-red-500'>Tu Balance</h3>
             <h1>${total}</h1>
         </div>
     )
